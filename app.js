b7638b6e479fec6cf2c41d1777ef7850e771d699
Jason Pien <jasonpien@jasons-mbp-1537.mynetworksettings.com>
2023-12-26 17:29:26 -0500

    and gate fix, current animation update, duplicate wires

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ function animateCurrent(wireId, currentId, trailId, duration) {
     var wireLength = wire.getTotalLength();
     var startTime = null;
 
+    function updateRadius(progress) {
+        // Define the radius decrease to 0 by the end of the animation
+        return 10 * (1 - progress); // Radius will be 10 at start and 0 at end
+    }
+
     function step(timestamp) {
         if (!startTime) startTime = timestamp;
         var elapsedTime = (timestamp - startTime) / 1000; // Convert to seconds
@@ -15,6 +20,9 @@ function animateCurrent(wireId, currentId, trailId, duration) {
         var point = wire.getPointAtLength(progress * wireLength);
         current.setAttribute('cx', point.x);
         current.setAttribute('cy', point.y);
+        
+        // Update the radius of the current
+        current.setAttribute('r', updateRadius(progress));
 
         // Update the trail to match the current's position
         var currentPos = progress * wireLength;
@@ -32,4 +40,6 @@ function animateCurrent(wireId, currentId, trailId, duration) {
 window.onload = function() {
     animateCurrent('wire1', 'current1', 'trail1', 5); // Animate the first wire
     animateCurrent('wire2', 'current2', 'trail2', 5); // Animate the second wire
+    animateCurrent('wire3', 'current3', 'trail3', 5); // Animate the first wire
+    animateCurrent('wire4', 'current4', 'trail4', 5); // Animate the second wire
 };
